a0d5659908ba642be4467e3e853e24d60188bdd8
Carli Martinez and Debbie Cravey <martinezcarli@gmail.com>
2015-02-18 10:43:26 -0800

    add functionality to deposit and withdraw funds

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+var Account = {
+  balance: 0,
+
+  withdraw: function(amount) {
+    this.balance = this.balance - amount;
+  },
+
+  deposit: function(amount) {
+    this.balance = this.balance = amount;
+  }
+};
--- spec/specs.js
+++ spec/specs.js
@@ -1,21 +1,17 @@
-describe('triangle', function(){
-  it("will prompt user for input if three side lengths are not received", function() {
-    expect(triangle('', '', '')).to.equal("Please enter three side lengths");
+describe('Account', function() {
+  describe('deposit()', function() {
+    it("adds the deposited amount to the account", function() {
+      var testAccount = Object.create(Account);
+      testAccount.deposit(50.00);
+      expect(testAccount.balance).to.equal(50.00);
+    });
   });
 
-  it("will determine if the side lengths entered create a valid triangle", function() {
-    expect(triangle(2, 2, 8)).to.equal("This is not a valid triangle")
-  });
-
-  it("will determine if the side lengths entered create an equilateral triangle", function() {
-    expect(triangle(10, 10, 10)).to.equal("Equilateral")
-  });
-
-  it("will determine if the side lengths entered create an isosceles triangle", function() {
-    expect(triangle(10, 10, 5)).to.equal("Isosceles")
-  });
-
-  it("will determine if the side lengths entered create a scalene triangle", function() {
-    expect(triangle(28, 18, 39)).to.equal("Scalene")
+  describe('withdraw()', function() {
+    it("subtracts the requested amount from the account", function() {
+      var testAccount = Object.create(Account);
+      testAccount.withdraw((49.00));
+      expect(testAccount.balance).to.equal(-49.00);
+    });
   });
 });
