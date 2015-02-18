e83c169cdf320568250f8a4e09272df2dc44cebc
Carli Martinez and Debbie Cravey <cinemaestro@yahoo.com>
2015-02-18 15:11:29 -0800

    format columns to show and hide initial account creation

--- js/scripts.js
+++ js/scripts.js
@@ -25,6 +25,7 @@ $(document).ready(function() {
     newAccount.balance = deposit + newAccount.balance;
     $("#balance").text(newAccount.balance);
     $(".result").show();
-    $("#hideinitial").hide();
+    $("#hideafterenter").hide();
+    $("#existing").show();
   });
 });
